5c784f6dddbfb80078bedf31922af60cc1624f5d
David Alarcon <davidalarcon@dorna.com>
2018-11-14 09:58:34 +0100

    Simple window with mock viewcontroller

---
+++ Sibiar/ViewViewModel/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Sibiar
+//
+//  Created by David Alarcon on 14/11/2018.
+//  Copyright © 2018 David Alarcon. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
